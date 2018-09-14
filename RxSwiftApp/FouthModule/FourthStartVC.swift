a5c2b2346975774d9b1131f65fd792ebf7ea9d9f
Андрей Коноплев <andrejkonoplev@MacBook-Air-Andrej.local>
2018-09-14 20:05:37 +0300

    GitEveny finished

--- RxSwiftApp/FouthModule/FourthStartVC.swift
+++ RxSwiftApp/FouthModule/FourthStartVC.swift
@@ -11,25 +11,49 @@ import RxSwift
 import RxCocoa
 import Kingfisher
 
+func cachedFileURL(_ filename: String) -> URL {
+    return FileManager.default.urls(for: .cachesDirectory, in: .allDomainsMask).first!.appendingPathComponent(filename)
+}
+
 class FourthStartVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    var refreshControl = UIRefreshControl()
     
     private let repo = "ReactiveX/RxSwift"
     private let events = Variable<[GitEvent]>([])
     private let bag = DisposeBag()
     
+    private let lastModified = Variable<NSString?>(nil)
+    private let modifiedFileURL = cachedFileURL("modified.txt")
+    private let eventFileURL = cachedFileURL("events.plist")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //FourthPlayGround.doThis()
+        FourthPlayGround.doThis()
+        
+        //write data from disk
+        let eventsArray = (NSArray.init(contentsOf: eventFileURL)) as? [[String: Any]] ?? []
+        lastModified.value = try? NSString(contentsOf: modifiedFileURL, usedEncoding: nil)
+        
+        events.value = eventsArray.flatMap(GitEvent.init)
+        setUpRefreshControl()
         navigationItem.title = repo
+        fetchEvents(repo: repo)
+    }
+    
+    func fetchEvents(repo: String) {
         let response = Observable.from([repo]).map { urlString -> URL in
             return URL(string: "https://api.github.com/repos/\(urlString)/events")!
-            }.map { (url) -> URLRequest in
-                return URLRequest(url: url)
+            }.map { [weak self] (url) -> URLRequest in
+                var request = URLRequest(url: url)
+                if let modifiedValue = self?.lastModified.value {
+                    request.addValue(modifiedValue as String, forHTTPHeaderField: "Last-Modified")
+                }
+                return request
             }.flatMap { (request) -> Observable<(response: HTTPURLResponse, data: Data)> in
                 return URLSession.shared.rx.response(request: request)
-        }.share(replay: 1, scope: .whileConnected)
+            }.share(replay: 1, scope: .whileConnected)
         
         response.filter { (response, _) -> Bool in
             return 200..<300 ~= response.statusCode
@@ -45,9 +69,22 @@ class FourthStartVC: UIViewController {
                 DispatchQueue.main.async {
                     self?.processEvents(newEvents)
                 }
-
+                
             })
             .disposed(by: bag)
+        
+        response.filter { (response, _) -> Bool in
+            return 200..<400 ~= response.statusCode
+            }.flatMap { (response, _) -> Observable<NSString?> in
+                guard let value = response.allHeaderFields["Last-Modified"] as? NSString else {
+                    return Observable.empty()
+                }
+            return Observable.just(value)
+            }.subscribe(onNext: { [weak self] modifiedHeader in
+                guard let strongSelf = self else { return }
+                strongSelf.lastModified.value = modifiedHeader
+                try? modifiedHeader?.write(to: strongSelf.modifiedFileURL, atomically: true, encoding: String.Encoding.utf8.rawValue)
+            }).disposed(by: bag)
     }
 }
 
@@ -59,6 +96,9 @@ extension FourthStartVC {
         }
         events.value = updateEvents
         self.tableView.reloadData()
+        self.refreshControl.endRefreshing()
+        let eventsArray = updateEvents.map({ $0.dictionary }) as NSArray
+        eventsArray.write(to: eventFileURL, atomically: true)
     }
 }
 
@@ -82,5 +122,21 @@ extension FourthStartVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableViewAutomaticDimension
+    }
+}
+
+extension FourthStartVC {
+    func setUpRefreshControl() {
+        refreshControl.backgroundColor = UIColor(white: 0.98, alpha: 1.0)
+        refreshControl.tintColor = UIColor.darkGray
+        refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        self.tableView.addSubview(refreshControl)
+    }
+    
+    @objc func refresh() {
+        DispatchQueue.global(qos: .background).async {
+            self.fetchEvents(repo: self.repo)
+        }
     }
 }
